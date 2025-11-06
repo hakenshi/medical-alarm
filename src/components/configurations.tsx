a4460407c5d6ff8e6629295e6dbdf974d6e326ec
hakenshi <fkdias025@gmail.com>
2025-11-06 16:24:11 -0300

    feat: atualizar layout da página de configurações para incluir uma lista

--- src/components/configurations.tsx
+++ src/components/configurations.tsx
@@ -2,6 +2,9 @@ export default function Configurations() {
   return (
     <div className="w-screen h-screen flex flex-col gap-5 justify-center items-center">
       <h1>Configurações</h1>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   )
 }
